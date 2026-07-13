1bdb393f8c158ad478bd27cd8f0322eb0fa50b4c
Dai En <tohdaien01@gmail.com>
2026-07-13 23:16:18 +0800

    adding starting files

---
+++ waypoint/waypoint/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  waypoint
+//
+//  Created by Toh Dai En on 13/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ waypoint/waypoint/waypointApp.swift
@@ -0,0 +1,17 @@
+//
+//  waypointApp.swift
+//  waypoint
+//
+//  Created by Toh Dai En on 13/7/26.
+//
+
+import SwiftUI
+
+@main
+struct waypointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
